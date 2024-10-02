a2e08cb1d6510b76fe9d926f6a0e6261fb4bc24e
Vitor Oliveira <melooliveira2003@gmail.com>
2024-10-01 23:12:53 -0300

    feat: add form component and edit components

--- src/pages/Contact/index.tsx
+++ src/pages/Contact/index.tsx
@@ -1,7 +1,25 @@
+import { FormContact } from "../../components/FormContact";
+
+import image from "../../assets/interiorDesigns/IMAGEM TOTAL2.png";
+import { Container } from "../../components/Container";
+
 export const Contact = () => {
   return (
-    <div>
-      <h2>Página de contato</h2>
-    </div>
+    <Container>
+      <div className="w-full p-8">
+        <h1 className="text-[40px] mb-4">Contato</h1>
+        <hr />
+        <div className="mt-8">
+          <div className="flex gap-6">
+            <div className="w-[30%]">
+              <img src={image} alt="" className="h-full object-cover" />
+            </div>
+            <div className="w-[70%]">
+              <FormContact />
+            </div>
+          </div>
+        </div>
+      </div>
+    </Container>
   );
 };
